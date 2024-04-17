014e30f649972d62a3293bb718d54e1fed8c6bda
Michal <sasxcxc@icloud.com>
2024-04-17 20:35:04 +0200

    Created repository

---
+++ src/main/java/com/crudapp/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.crudapp.fullstackbackend.repository;
+
+import com.crudapp.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
+
